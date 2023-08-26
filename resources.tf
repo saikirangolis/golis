ccec6b20ed62c4188317643b91c8517219dac984
Kiran <kiran@pawprint.eco>
2023-08-26 16:39:39 +0100

    4 commit

--- resources.tf
+++ resources.tf
@@ -1,3 +1,22 @@
 resource "aws_vpc" "golivpc" {
-    cidr_block = "var.vpc_cidr_block"
+    cidr_block = var.vpc_cidr_block
 }
+resource "aws_subnet" "public_subnet" {
+    cidr_block = var.public_subnet_cidr_block
+    vpc_id = aws_vpc.golivpc.id
+}
+resource "aws_subnet" "private_subnet" {
+    cidr_block = var.private_subnet_cidr_block
+    vpc_id = aws_vpc.golivpc.id
+}
+resource "aws_instance" "sai" {
+    ami = ami-06f621d90fa29f6d0
+    tags = { 
+        name = "mywebserver"
+    }
+}
+
+
+ 
+
+    
--- variables.tf
+++ variables.tf
@@ -3,6 +3,14 @@ variable "region" {
     description = "aws-region"
 }
 variable "vpc_cidr_block" {
-    #default = "192.168.0.0/16"
+    default = "192.168.0.0/16"
     description = "vpc cidr.block"
+}
+variable "public_subnet_cidr_block" {
+    default = "192.168.0.10/24"
+    description = "public subnet cidr block"
+}
+variable "private_subnet_cidr_block" {
+    default = "192.168.0.11/24"
+    description = "private subnet cidr block"
 }
